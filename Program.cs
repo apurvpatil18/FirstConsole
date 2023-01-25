358852d34e603f31a52365ac37d938d34dee526f
apurvpatil18 <patilapurv18@gmail.com>
2023-01-25 15:59:40 +0530

    -A

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FirstConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(" T4");
+        }
+    }
+}
